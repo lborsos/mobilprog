c9b63f8f5486383bebb4f985e31bad2cc3e41c34
Borsos Laszlo <lborsos@gmail.com>
2025-05-20 11:28:15 +0300

    mp2_2 completed

--- f4mqfm_mp2_2/app/build.gradle.kts
+++ f4mqfm_mp2_2/app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
